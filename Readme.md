2f9cb0b8280d86fb237ee8eb6f994586c52a2801
Alexey Belyakov <qqrm@mail.ru>
2023-09-08 15:17:16 +0300

    reacatoring and readme

---
+++ Readme.md
@@ -0,0 +1,32 @@
+# Rust Project with Actix-web and Async Tasks
+
+This repository showcases an example implementation using Actix-web for building asynchronous web servers. It demonstrates the handling of concurrent tasks, making requests to external servers, and state management with mutex locks.
+
+## Prerequisites
+
+- Rust and Cargo installed
+
+## Setup &amp; Running
+
+1. Clone the repository:
+```bash
+git clone https://github.com/qqrm/Faulty-Server-Problem
+```
+
+2. Navigate to the project directory:
+```bash
+cd Faulty-Server-Problem
+```
+
+3. Build and run the project:
+```bash
+cargo build && cargo run
+```
+
+## Contributions
+
+Feel free to submit pull requests or open issues to enhance the project.
+
+## License
+
+This project is under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,16 @@
-#[macro_use]
-extern crate serde_derive;
-#[macro_use]
-extern crate lazy_static;
-#[macro_use]
-extern crate actix;
-#[macro_use]
-extern crate actix_web;
-
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use envmnt::{ExpandOptions, ExpansionType};
+use envmnt;
 use nanoid::nanoid;
 use reqwest;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::collections::{HashMap, VecDeque};
 use std::sync::Mutex;
-use std::time::Duration;
-use web::Json;
+use actix_rt::time::delay_for;
 
 type Id = String;
 
+/// Represents the status of a client.
 #[derive(Serialize, Deserialize)]
 struct ClientStatus {
     status: Mutex<String>,
@@ -28,7 +19,8 @@ struct ClientStatus {
 }
 
 impl ClientStatus {
-    fn new() -> ClientStatus {
+    /// Creates a new client status with default values.
+    fn new() -> Self {
         ClientStatus {
             status: Mutex::new("NEW".to_string()),
             successful_responses_count: 0,
@@ -36,152 +28,103 @@ impl ClientStatus {
         }
     }
 
+    /// Adds a value to the sum and increases the successful responses count.
     pub fn add(&mut self, n: i64) {
         self.successful_responses_count += 1;
-        *self.sum.get_mut().unwrap() += n;
+        *self.sum.get_mut().expect("Failed to lock sum mutex") += n;
     }
 
-    fn set_status(&mut self, status: String) {
-        *self.status.get_mut().unwrap() = status.to_string();
+    /// Sets the status of the client.
+    fn set_status(&mut self, status: &str) {
+        *self.status.get_mut().expect("Failed to lock status mutex") = status.to_string();
     }
 
+    /// Marks the client status as in progress.
     pub fn set_in_progress(&mut self) {
-        self.set_status("IN_PROGRESS".to_string());
+        self.set_status("IN_PROGRESS");
     }
+
+    /// Marks the client status as finished.
     pub fn set_fin(&mut self) {
-        self.set_status("FINISH".to_string());
+        self.set_status("FINISH");
     }
 }
 
+/// Parameters required to start a run.
 #[derive(Serialize, Deserialize)]
 struct StartParams {
     seconds: u64,
 }
 
+/// Represents an income number.
 #[derive(Serialize, Deserialize)]
 struct IncomeNum {
     value: i32,
 }
 
+/// Contains application state data, including various counters and client statuses.
 struct AppState {
     max_pend: Mutex<i64>,
     max_runs: Mutex<i64>,
     clients: Mutex<HashMap<Id, ClientStatus>>,
     current_clients: Mutex<i64>,
     current_clients_pends: Mutex<i64>,
-    pends: Mutex<std::collections::VecDeque<(String, u64)>>,
+    pends: Mutex<VecDeque<(String, u64)>>,
 }
 
+/// Endpoint to handle requests to start runs.
 #[post("/runs")]
 async fn runs(data: web::Data<AppState>, start_params: Json<StartParams>) -> impl Responder {
     let id = nanoid!();
 
     let client = ClientStatus::new();
-    data.clients.lock().unwrap().insert(id.clone(), client);
+    data.clients.lock().expect("Failed to lock clients mutex").insert(id.clone(), client);
 
-    if *data.current_clients.lock().unwrap() < *data.max_runs.lock().unwrap() {
-        let fut = task(data.clone(), id.clone(), start_params.seconds);
-
-        actix_web::rt::spawn(fut);
-
-        return HttpResponse::Ok()
-            .content_type("application/json")
-            .body(json!({ "id": id }));
-    } else if *data.current_clients_pends.lock().unwrap() < *data.max_pend.lock().unwrap() {
-        *data.current_clients_pends.lock().unwrap() += 1;
-        data.pends
-            .lock()
-            .unwrap()
-            .push_back((id.clone(), start_params.seconds));
-
-        return HttpResponse::Ok()
-            .content_type("application/json")
-            .body(json!({ "id": id }));
+    // Check if new tasks can be spawned or should be pending.
+    if *data.current_clients.lock().expect("Failed to lock current_clients mutex") < *data.max_runs.lock().expect("Failed to lock max_runs mutex") {
+        actix_web::rt::spawn(task(data.clone(), id.clone(), start_params.seconds));
+        HttpResponse::Ok().content_type("application/json").body(json!({ "id": id }))
+    } else if *data.current_clients_pends.lock().expect("Failed to lock current_clients_pends mutex") < *data.max_pend.lock().expect("Failed to lock max_pend mutex") {
+        *data.current_clients_pends.lock().expect("Failed to lock current_clients_pends mutex") += 1;
+        data.pends.lock().expect("Failed to lock pends mutex").push_back((id.clone(), start_params.seconds));
+        HttpResponse::Ok().content_type("application/json").body(json!({ "id": id }))
     } else {
         HttpResponse::TooManyRequests().finish()
     }
 }
 
+/// Endpoint to fetch information about a specific run using its ID.
 #[get("/runs/{id}")]
 async fn run_info(data: web::Data<AppState>, path: web::Path<(String,)>) -> impl Responder {
     let id = path.into_inner().0;
-    let js_resp = match data.clients.lock().unwrap().get(&id) {
+    let clients = data.clients.lock().expect("Failed to lock clients mutex");
+    match clients.get(&id) {
         Some(client) => {
-            json!({
-                "status": (*client).status,
-                "successful_responses_count": (*client).successful_responses_count,
-                "sum": (*client).sum
-            })
+            HttpResponse::Ok().content_type("application/json").body(json!({
+                "status": *client.status.lock().expect("Failed to lock client status"),
+                "successful_responses_count": client.successful_responses_count,
+                "sum": *client.sum.lock().expect("Failed to lock client sum")
+            }))
         }
-        None => json!({ "err": "id not found" }),
-    };
-
-    HttpResponse::Ok()
-        .content_type("application/json")
-        .body(js_resp)
+        None => HttpResponse::Ok().content_type("application/json").body(json!({ "err": "id not found" }))
+    }
 }
 
+/// Represents a task or run, which makes requests to an external server.
 async fn task(data: web::Data<AppState>, id: Id, seconds: u64) {
     let url = "http://faulty-server-htz-nbg1-1.wvservices.exchange:8080";
     let header = "X-Run-Id";
 
-    data.clients
-        .lock()
-        .unwrap()
-        .get_mut(&id)
-        .unwrap()
-        .set_in_progress();
-    *data.current_clients.lock().unwrap() += 1;
+    data.clients.lock().expect("Failed to lock clients mutex").get_mut(&id).expect("Client not found").set_in_progress();
+    *data.current_clients.lock().expect("Failed to lock current_clients mutex") += 1;
 
     for _ in 1..=seconds {
-        let res = reqwest::Client::new()
-            .get(url)
-            .header(header.clone(), id.clone())
-            .send()
-            .await
-            .unwrap();
-
+        let res = reqwest::Client::new().get(url).header(header, &id).send().await.expect("Failed to send request");
         if res.status().is_success() {
-            let js = res.json::<IncomeNum>().await.unwrap();
-            data.clients
-                .lock()
-                .unwrap()
-                .get_mut(&id)
-                .unwrap()
-                .add(js.value as i64);
+            let js = res.json::<IncomeNum>().await.expect("Failed to deserialize IncomeNum");
+            data.clients.lock().expect("Failed to lock clients mutex").get_mut(&id).expect("Client not found").add(js.value as i64);
         }
-
-        std::thread::sleep(Duration::from_secs(1)); //так?🤷‍♂️
+        delay_for(Duration::from_secs(1)).await;  // Asynchronous sleep
     }
 
-    data.clients.lock().unwrap().get_mut(&id).unwrap().set_fin();
-    *data.current_clients.lock().unwrap() -= 1;
-
-    if data.pends.lock().unwrap().len() != 0 {
-        *data.current_clients_pends.lock().unwrap() -= 1;
-        let pend = data.pends.lock().unwrap().pop_front().unwrap();
-        actix_web::rt::spawn(task(data, pend.0, pend.1));
-    }
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let data = web::Data::new(AppState {
-        max_pend: Mutex::new(envmnt::get_i64("MAX_PENDING_RUNS", 5)),
-        max_runs: Mutex::new(envmnt::get_i64("MAX_CONCURRENT_RUNS", 5)),
-        clients: Mutex::new(HashMap::new()),
-        current_clients: Mutex::new(0),
-        current_clients_pends: Mutex::new(0),
-        pends: Mutex::new(std::collections::VecDeque::new()),
-    });
-
-    HttpServer::new(move || {
-        App::new()
-            .app_data(data.clone())
-            .service(runs)
-            .service(run_info)
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
-}
+    data.clients.lock().expect("Failed to lock clients mutex").get_mut(&id).
